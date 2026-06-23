bd1c5f002c034739b60ea2ece3de43a6ed2c0a54
Amisha103 <bhatiaamisha74@gmail.com>
2026-06-23 22:58:35 +0530

    Logistic Regression performed

--- model.py
+++ model.py
@@ -7,4 +7,26 @@ import warnings
 warnings.filterwarnings('ignore')
 df = sns.load_dataset('titanic')
 print(df.head())
+df.drop(['deck', 'embark_town', 'alive', 'class', 'who', 'adult_male'], axis=1, inplace=True)
 
+print(df.info())
+df['age'].fillna(df['age'].mean(), inplace=True)
+df.dropna(subset=['embarked'], inplace=True)
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+df['sex'] = le.fit_transform(df['sex'])
+df = df.astype(int)
+
+X = df.drop('survived', axis=1)
+y = df['survived']
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+ 
