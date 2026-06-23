ab04dcd2eb53fab8d20983b3d4563eb7fe6254fb
Amisha103 <bhatiaamisha74@gmail.com>
2026-06-23 22:42:28 +0530

    loading dependencies

---
+++ model.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+
+warnings.filterwarnings('ignore')
+df = sns.load_dataset('titanic')
+print(df.head())
+
